dac9fdde89db2d52897c0d92dca22aa115e0d38f
DiegoMZurita <thedaired@gmail.com>
2025-12-01 23:16:31 -0300

    feat: agregar validaciones en PersonalInfo y manejo global de errores

---
+++ src/main/java/com/portafolio/mi_portafolio_backend/exception/ValidationException.java
@@ -0,0 +1,13 @@
+package com.portafolio.mi_portafolio_backend.exception;
+
+import lombok.Getter;
+import org.springframework.validation.BindingResult;
+
+@Getter
+public class ValidationException extends RuntimeException {
+    private final BindingResult bindingResult;
+    public ValidationException(BindingResult bindingResult) {
+        super("Error de validación: Se encontraron " + bindingResult.getErrorCount());
+        this.bindingResult = bindingResult;
+    }
+}
--- src/main/java/com/portafolio/mi_portafolio_backend/service/PersonalInfoServiceImpl.java
+++ src/main/java/com/portafolio/mi_portafolio_backend/service/PersonalInfoServiceImpl.java
@@ -1,23 +1,37 @@
 package com.portafolio.mi_portafolio_backend.service;
 
+import com.portafolio.mi_portafolio_backend.exception.ValidationException;
 import com.portafolio.mi_portafolio_backend.model.PersonalInfo;
 import com.portafolio.mi_portafolio_backend.repository.IPersonalInfoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
 
+import org.springframework.validation.Validator;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class PersonalInfoServiceImpl implements IPersonalInfoService {
 
     private final IPersonalInfoRepository personalInfoRepository;
+    private final Validator validator;
 
-    public PersonalInfoServiceImpl(IPersonalInfoRepository personalInfoRepository) {
-        this.personalInfoRepository = personalInfoRepository;
-    }
+//    Como estamos utilizando lombok no es necesario crear el constructor
+//    public PersonalInfoServiceImpl(IPersonalInfoRepository personalInfoRepository,  Validator validator) {
+//        this.personalInfoRepository = personalInfoRepository;
+//        this.validator = validator;
+//    }
 
     @Override
     public PersonalInfo save(PersonalInfo personalInfo) {
+        BindingResult result = new BeanPropertyBindingResult(personalInfo, "personalInfo");
+        validator.validate(personalInfo, result);
+        if(result.hasErrors()){
+            throw new ValidationException(result);
+        }
         return personalInfoRepository.save(personalInfo);
     }
 
