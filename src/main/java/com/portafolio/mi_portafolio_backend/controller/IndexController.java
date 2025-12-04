e2aa782a96b2fa6ff4b560bc12e1bd61ed8588a0
DiegoMZurita <thedaired@gmail.com>
2025-12-04 11:03:56 -0300

    agregar secciones dinámicas (Education, Experience, Skills) como fragments e integrar estilos e imágenes

--- src/main/java/com/portafolio/mi_portafolio_backend/controller/IndexController.java
+++ src/main/java/com/portafolio/mi_portafolio_backend/controller/IndexController.java
@@ -1,14 +1,31 @@
 package com.portafolio.mi_portafolio_backend.controller;
 
+import com.portafolio.mi_portafolio_backend.service.IEducationService;
+import com.portafolio.mi_portafolio_backend.service.IExperienceService;
+import com.portafolio.mi_portafolio_backend.service.IPersonalInfoService;
+import com.portafolio.mi_portafolio_backend.service.ISkillService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final IPersonalInfoService  personalInfoService;
+    private final IEducationService educationService;
+    private final ISkillService skillService;
+    private final IExperienceService  experienceService;
+
     @GetMapping("/")
-    public String showIndex(){
+    public String showIndex(Model model){
         System.out.println("Mostrando la página de inicio");
+
+        model.addAttribute("personalInfo", personalInfoService.findAll());
+        model.addAttribute("educationList", educationService.findAll());
+        model.addAttribute("skills", skillService.findAll());
+        model.addAttribute("experienceList", experienceService.findAll());
         return "index";
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,13 +2,15 @@ INSERT INTO personal_info (first_name, last_name, title, profile_description, pr
 ('Diego', 'Martínez', 'Full Stack Developer', 'Apasionado por el desarrollo web con experiencia en Java, Spring Boot y React. Disfruto construyendo soluciones robustas y escalables.', 'img/profile-placeholder.jpg', 0, 'diego.marzurita@gmail.com', '+569455075968', 'https://www.linkedin.com/in/diego-mart%C3%ADnez-zurita-0b8200367/', 'https://github.com/DiegoMZurita');
 
 INSERT INTO skills (name, level_percentage, icon_class, personal_info_id) VALUES
-('Java', 90, 'fab fa-java', 1),
-('Spring Boot', 85, 'fas fa-leaf', 1),
-('PostgreSQL', 80, 'fas fa-database', 1),
-('HTML', 95, 'fab fa-html5', 1),
-('CSS', 90, 'fab fa-css3-alt', 1),
-('JavaScript', 75, 'fab fa-js-square', 1),
-('React', 70, 'fab fa-react', 1);
+('Java', 90, 'img/logos/java.png', 1),
+('Spring Boot', 85, 'img/logos/spring.png', 1),
+('Python', 90, 'img/logos/python.png', 1),
+('Django', 80, 'img/logos/django.png', 1),
+('PostgreSQL', 80, 'img/logos/servidor-sql.png', 1),
+('HTML', 95, 'img/logos/html-5.png', 1),
+('CSS', 90, 'img/logos/css-3.png', 1),
+('JavaScript', 75, 'img/logos/js.png', 1);
+
 
 INSERT INTO educations (degree, institution, start_date, end_date, description, personal_info_id) VALUES
 ('Técnico en Desarrollo de Aplicaciones Multiplataforma', 'IPLACEX', '2022-03-01', '2025-12-15', 'Especialización en desarrollo de aplicaciones multiplataforma.', 1),
